9bc034a4f589affeae882c3b791eccfcce2d0065
Ishita Singh <ishitasingh0804@gmail.com>
2026-06-15 16:59:50 +0530

    feat: add reusable Navbar Hero Card and Footer components

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ function Hero() {
     <section className="bg-gradient-to-r from-green-500 to-emerald-700 text-white py-24">
       <div className="max-w-6xl mx-auto text-center px-6">
         <h1 className="text-5xl font-bold mb-6">
-          Reduce Food Waste with SmartPantry
+          Manage Your Pantry Smarter with SmartPantry{" "}
         </h1>
 
         <p className="text-xl mb-8">
